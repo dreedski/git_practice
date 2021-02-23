b469f91c190feea4a122a4d4213ea6197a554384
dreedski <dreedski@hotmail.com>
2021-02-23 16:12:59 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner in = new Scanner(System.in);
+        dateClass dC = new dateClass();
+
+
+    }
+}
---
+++ dateClass.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+package com.company;
+
+public class dateClass {
+    Scanner in = new Scanner(System.in);
+
+
+
+
+}
